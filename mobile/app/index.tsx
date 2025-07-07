eb7f497bcd45429d319169cfac7c0066ccb2b337
Emmanuel Acheampong <e04acheampong@gmail.com>
2025-07-07 13:56:45 +0000

    initial setup

---
+++ mobile/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className='bg-white flex-1'>
+      <Text  className='text-green-300'> the index</Text>
+    </View>
+  )
+}
+
+export default index
